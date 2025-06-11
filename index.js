eb7efaae83ba8d1e4a312acf32917bee8376a02f
Sagar Davara <sagardspeed2@gmail.com>
2025-06-11 09:42:04 +0530

    update port

--- index.js
+++ index.js
@@ -78,13 +78,13 @@ app.post("/send_mail", async (req, res) => {
 
       const transporter = nodemailer.createTransport({
         service: "Gmail",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
           user: "kandarp@miraiminds.co",
           pass: "knkwlzqncbiwjuex", // App password
         },
-        family: 4,
+        family: 6,
         debug: true,
         logger: true,
       });
